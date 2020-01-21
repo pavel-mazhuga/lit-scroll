09ffd0e2c60edd128118e9997f67a81a3650146b
pavel-mazhuga <pavelmazhuga@yandex.ru>
2020-01-21 22:01:36 +0700

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import typescript from 'rollup-plugin-typescript2';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import { eslint } from 'rollup-plugin-eslint';
+import pkg from './package.json';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'esm',
+        },
+    ],
+    external,
+    plugins: [
+        resolve({ extensions }),
+        eslint(),
+        typescript({
+            typescript: require('typescript'),
+        }),
+        terser(),
+    ],
+};
