0aa25e9635a2b59ac7e7a3d0f98371e62a3265b0
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2020-04-09 08:45:16 +0700

    fix umd name

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default {
         {
             file: pkg.main,
             format: 'umd',
-            name: 'LitScroll',
+            name: 'createLitScroll',
         },
         {
             file: pkg.module,
